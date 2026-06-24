9e6cda235bdd78fb258500d85d7d2ffce2d26c96
Ian Kenneth G. Mendoza <ian.mendoza@reliancehealth.com.ph>
2026-06-24 14:43:06 +0800

    Clean laravel app api

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
